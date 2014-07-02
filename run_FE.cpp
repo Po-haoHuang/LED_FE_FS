e96b655db4aae35d7ffcce8dac778e70698cf17b
Po-haoHuang <hr904217@gmail.com>
2014-07-02 17:41:08 +0800

    run_FE_proto

--- run_FE.cpp
+++ run_FE.cpp
@@ -4,10 +4,31 @@
 /* 1:no segmentation  2-3:segmentation, 作法不同,
 case 2須更改FeatureExtraction方法, case 3適用於RUN數少，不增加feature數時使用
 */
-enum segmentPara{disable,enable,enablerunless};
+enum segmentPara{disable,enable,enableless};
+const char* fileName="cycle";
 int main(){
-	segmentPara seg=1;
-	
+	segmentPara seg=disable;
+
+	//read from result of dataEtract
+	for(int count=1;count<=cycleNum;count++){	
+		FILE *fPtr=fopen(strcat(fileName+itoa(count)+".txt","r");
+		
+		if(!fPtr){
+    	    printf("Cannot open file: %s\n",fileName);
+        	exit(1);
+    	}
+	}
+	switch (seg){
+		case disable:
+			
+		case enable:
+		
+		case enableless:
+			
+		
+		
+		
+	}
 	
 	
 }
