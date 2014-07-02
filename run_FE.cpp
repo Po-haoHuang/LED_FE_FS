4dc777563a32f073cebd565ef21ce0df6b80c01c
Po-haoHuang <hr904217@gmail.com>
2014-07-02 16:50:24 +0800

    building run_FE

---
+++ run_FE.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/* 1:no segmentation  2-3:segmentation, 作法不同,
+case 2須更改FeatureExtraction方法, case 3適用於RUN數少，不增加feature數時使用
+*/
+enum segmentPara{disable,enable,enablerunless};
+int main(){
+	segmentPara seg=1;
+	
+	
+	
+}
