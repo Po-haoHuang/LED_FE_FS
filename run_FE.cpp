01a7d555bfca17bbf5a03af9325e012bac5e5fa2
Po-haoHuang <hr904217@gmail.com>
2014-07-04 11:29:35 +0800

    modified FE to c++,single file FE done

--- run_FE.cpp
+++ run_FE.cpp
@@ -4,8 +4,14 @@
 #include<math.h>
 #include <gsl/gsl_sort.h>
 #include<gsl/gsl_statistics_double.h>
-#define attrnum 6
-#define datasize 6
+#include<iostream>
+#include<vector>
+
+#define attrNum 6
+#define dataSize 6
+#define outputNum 12
+
+using namespace std;
 
 double rowData[6][6]={
 {-5.701741013,100.016276,1199.951172,5000,5.502386154,0.009155553},
@@ -23,10 +29,14 @@ double *FeatureExtraction(double* cleanData);
 
 int main(){
 
+
+
+
+
 	segmentPara seg = disable;
-	double* result;
-	result = calloc(attrnum,sizeof(double));
-	double temp[6];
+	double* tempResult;
+	tempResult = (double*)calloc(attrNum,sizeof(double));
+	double temp[dataSize];
 	//read from result of dataEtract
 	/*for(int count = 1;count<=cycleNum;count++){	
 		FILE *fPtr = fopen(strcat(fileName+itoa(count,buffer,10)+".txt"),"r");
@@ -42,21 +52,35 @@ int main(){
 	}*/
 	switch (seg){
 		case disable://no segmentation
-
-			for(int i = 0;i < 6;i++)
-				temp[i] = rowData[i][0];			
-			result = FeatureExtraction(temp);
-			for(int i = 0;i < 12;i++)
-				printf("%lf\t",result[i]);
+		
+			vector<double> *singleResult = new vector<double> [outputNum];
+			for(int j = 0;j < attrNum;j++){
+			
+				for(int i = 0;i < dataSize;i++)
+					temp[i] = rowData[i][j];			
+				tempResult = FeatureExtraction(temp);
+			
+				for(int i = 0;i < outputNum;i++){
+					singleResult[i].push_back(tempResult[i]);
+					//printf("%lf\t",singleResult[i][j]);
+				}
+				//printf("\n");
+			}
+			for(int i = 0;i < outputNum;i++){
+				for(int j = 0;j < attrNum;j++)
+				printf("%lf\t",singleResult[i][j]);
+				cout<<endl;
+			}
+			
 			//GenParaFeatureNameSet();			
 			break;
 			
-		case enable://segmentation
+		/*case enable://segmentation
 			
 			//DataSegmentation();
 			//FeatureExtraction_m();
 			//GenParaFeatureNameSet_m();
-			break;
+			break;*/
 		
 	}
 	
@@ -67,41 +91,44 @@ double *FeatureExtraction(double* cleanData){
 	//check if it is empty not implement yet
 
 	
-	int length = datasize;//not implement yet
+	int length = dataSize;//not implement yet
 
 	static double f[12];
 	double tempData[6];
 	
+	
 	//tempData = cleanData^2;
-
 	for(int i = 0; i++; i<length)
-		tempData[i] = pow(cleanData[i], 2);
-	f[0] = 0;
-	f[1] = 0;
-	
-	
-	//three para data_array,element size(#double),#elements	
+		tempData[i] = pow(cleanData[i], 2);		
+		
+	f[0] = 0;//peak in matlab not implement
+	f[1] = 0;	
+	//three para for gsl func (data_array,element size(#double),#elements)	
 	f[2] = gsl_stats_mean (cleanData, 1, length);
 	f[3] = gsl_stats_variance (cleanData, 1, length);
 	f[4] = gsl_stats_skew (cleanData, 1, length);
 	f[5] = gsl_stats_kurtosis (cleanData, 1, length);
-	f[6] = sqrt (gsl_stats_mean (tempData, 1, length));
+	f[6] = sqrt (gsl_stats_mean (tempData, 1, length));//rms
 	f[7] = gsl_stats_max (cleanData, 1, length);
 	f[8] = gsl_stats_min (cleanData, 1, length);
-	f[9] = f[7] - f[8];
+	f[9] = f[7] - f[8];//range
 	gsl_sort (cleanData, 1, length);//sort before iqr
+	//iqr
 	f[10] = gsl_stats_quantile_from_sorted_data (cleanData,1, length, 0.75)
 			- gsl_stats_quantile_from_sorted_data (cleanData,1, length, 0.25);
-	f[11] = gsl_stats_sd (cleanData, 1, length);
+	f[11] = gsl_stats_sd (cleanData, 1, length);//std
 	for(int i = 0;i < 12;i++){//set 0 if nan
-		//if(isnan(f[i]))
-		//	f[i] = 0;
-		printf("%lf\t",f[i]);
+		if(isnan(f[i]))
+			f[i] = 0;
 	}
 	return f;
 	
 	
 }
+
+
+
+
 /*
 FeatureExtraction_m(){
 	//check if it is empty not implement yet
