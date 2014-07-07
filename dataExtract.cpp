5d7d177d777c664af797c731898a9cacff5485fd
Cory Lee <cory8249@gmail.com>
2014-07-07 10:36:47 +0800

    FileData read ok!

--- dataExtract.cpp
+++ dataExtract.cpp
@@ -12,27 +12,27 @@ const int MAX_CYCLE_INFO_ARRAY_LENGTH = 256;
 
 using namespace std;
 
-const char cycleListFileName[] = "use_file_list.csv";
-const char dataDirName[] = "dp_variable_selection";
-const char selectionPath[] = ".\\dp_variable_selection\\selection\\";  // Windows only
+const string cycleListFileName = "use_file_list.csv";
+const string dataDir = ".\\dp_variable_selection\\";
+const string dataSelectionDir = ".\\dp_variable_selection\\selection\\";  // Windows only
 
 struct FileData{
     int id;
     int originalFileId;
     string fileName;
     vector<vector<double> > dataVector;
-    vector<string> *timeStamp;
-    vector<string> *attributeTypeVector;
+    vector<string> timeStamp;
+    vector<string> attributeTypeVector;
 };
 
 struct CycleData {
     double cycle;
-    vector<FileData> *fileDataVector;
+    vector<FileData> fileDataVector;
 };
 
 FileData& getFileById(vector<CycleData> &cycleDataVector, int id){
     for(unsigned i=0; i<cycleDataVector.size(); i++){  // search corresponding FileData
-        vector<FileData> &vec = *cycleDataVector[i].fileDataVector;
+        vector<FileData> &vec = cycleDataVector[i].fileDataVector;
         for(unsigned j=0; j<vec.size(); j++){
             if(vec[j].id==id){
                 return vec[j];
@@ -41,6 +41,23 @@ FileData& getFileById(vector<CycleData> &cycleDataVector, int id){
         }
     }
     return *(new FileData);
+}
+
+int getNextFileNo(vector<CycleData> &cycleDataVector)
+{
+    static unsigned cycle = 0;
+    static unsigned file = 0;
+    int nextFileNo = -1;
+    if(cycle < cycleDataVector.size()) {
+        if(file >= cycleDataVector[cycle].fileDataVector.size()) {
+            cycle++;
+            file=0;
+        }
+        if(cycle < cycleDataVector.size())
+            nextFileNo = cycleDataVector[cycle].fileDataVector[file].originalFileId;
+        file++;
+    }
+    return nextFileNo;
 }
 
 void extractList(const char *listFileName, vector<CycleData> &cycleDataVector)
@@ -64,7 +81,6 @@ void extractList(const char *listFileName, vector<CycleData> &cycleDataVector)
     while(inFile.getline(lineBuffer, LINE_BUFFER_SIZE)) {
 
         CycleData cycleData;  // create a new element
-        cycleData.fileDataVector = new vector<FileData>;
 
         // split the line by comma (CSV file)
         pch = strtok (lineBuffer,",");
@@ -73,7 +89,7 @@ void extractList(const char *listFileName, vector<CycleData> &cycleDataVector)
         while(pch != NULL) {
             FileData fileData;
             fileData.originalFileId = atof(pch);
-            cycleData.fileDataVector->push_back(fileData);
+            cycleData.fileDataVector.push_back(fileData);
             pch = strtok (NULL, ",");
         }
         #ifdef PRINT_LIST
@@ -88,29 +104,55 @@ void extractList(const char *listFileName, vector<CycleData> &cycleDataVector)
     inFile.close();
 }
 
-void singleFileExtract(const char *fileName)
+void copyToSelection(vector<CycleData> &cycleDataVector){
+    // search data directory
+    cout << "Starting copy ..." << endl;
+    DIR *dir;
+    struct dirent *ent;
+    int nextFileNo = getNextFileNo(cycleDataVector);
+    int fileNameIndex = -1;
+    char fileNameIndexStr[5];
+    if ((dir = opendir (dataDir.c_str())) != NULL) {
+        cout << "dataDirName: " << dataDir << endl;
+        while ((ent = readdir (dir)) != NULL){
+            if(strstr(ent->d_name, "run")==NULL) continue;  // exclude other file
+            strncpy(fileNameIndexStr, ent->d_name+3,4); // get file serial number
+            fileNameIndexStr[4]=0;
+            fileNameIndex = atoi(fileNameIndexStr);
+            // cout << "nextFileNo: " << nextFileNo << endl;
+            if(fileNameIndex==nextFileNo){  // file id matched
+                string cmd = "copy /y \"" + dataDir + ent->d_name + "\"";
+                cmd += " \"" + dataSelectionDir + ent->d_name + "\"" ;
+                system(cmd.c_str());
+                nextFileNo = getNextFileNo(cycleDataVector);
+            }
+        }
+        closedir (dir);
+    } else {
+        // could not open directory
+        perror ("");
+        return;
+    }
+}
+
+void singleFileExtract(const char *fileName, FileData &fileData)
 {
     // open file
-    ifstream inFile(fileName, ios::in);
+    ifstream inFile((dataSelectionDir+fileName).c_str(), ios::in);
     if(!inFile) {
-        printf("Cannot open file: %s\n",fileName);
+        printf("Error! Cannot open file: %s\n",(dataSelectionDir+fileName).c_str());
     }
-    // cout << fileName << endl;
+    cout << "File ID: " << fileData.originalFileId << "  extracting ...";
+
 
     char lineBuffer[LINE_BUFFER_SIZE];
     char *pch;
-
-    // Initialize
-    FileData fileData;
-    //fileData.dataVector = new vector<vector<double> >;
-    fileData.attributeTypeVector = new vector<string>;
-    fileData.timeStamp = new vector<string>;
 
     // read first line (attribute type)
     inFile.getline(lineBuffer, LINE_BUFFER_SIZE);
     pch = strtok (lineBuffer,",");
     while (pch != NULL) {
-        fileData.attributeTypeVector->push_back(pch);
+        fileData.attributeTypeVector.push_back(pch);
         // cout << fileData.attributeTypeVector->back() << " \t";
         pch = strtok (NULL, ",");
     }
@@ -121,71 +163,62 @@ void singleFileExtract(const char *fileName)
         dataInLine.reserve(8);
         // split the line by comma
         pch = strtok (lineBuffer,",");
-        fileData.timeStamp->push_back(pch);
+        fileData.timeStamp.push_back(pch);
         pch = strtok (NULL, ",");
-        for(unsigned i=0; i<fileData.attributeTypeVector->size()-1; i++) {
+        for(unsigned i=0; i<fileData.attributeTypeVector.size()-1; i++) {
             dataInLine.push_back(atof(pch));
             pch = strtok (NULL, ",");
         }
         fileData.dataVector.push_back(dataInLine);
     }
+    inFile.close();
+
+    cout << " read " << fileData.dataVector.size() << " lines" << endl;
     #ifdef PRINT_FILE
-    cout << "In file " << fileName << " read " << fileData.dataVector.size() << " lines" << endl;
-    for(int i=0; i<10; i++){
+    /*for(int i=0; i<10; i++){
         for(int j=0; j<fileData.dataVector[i].size(); j++){
             cout << fileData.dataVector[i][j] << " \t";
         }
         cout << endl;
-    }
+    }*/
     #endif
-    inFile.close();
 }
 
-int getNextFileNo(vector<CycleData> &cycleDataVector)
-{
-    static unsigned cycle = 0;
-    static unsigned file = 0;
-    int nextFileNo = -1;
-    if(cycle < cycleDataVector.size()) {
-        if(file >= cycleDataVector[cycle].fileDataVector->size()) {
-            cycle++;
-            file=0;
-        }
-        if(cycle < cycleDataVector.size())
-            nextFileNo = (*cycleDataVector[cycle].fileDataVector)[file].originalFileId;
-        file++;
-    }
-    return nextFileNo;
-}
-
-void cycleExtract(vector<CycleData> &cycleDataVector)
+void cycleExtract(vector<CycleData> &cycleDataVector, bool doCopy)
 {
     vector<string> fileNameVector;
 
     static int newIdCounter = 0; // give each selected file an unique new ID
 
     // choose file on list
-    extractList(cycleListFileName, cycleDataVector);
-    printf("cycleDataVector.size() = %d\n", cycleDataVector.size());
+    extractList(cycleListFileName.c_str(), cycleDataVector);
+    cout << "cycleDataVector.size() = " << cycleDataVector.size() << endl;
 
+    if(doCopy){
+        // copy data to selection if first time run
+        copyToSelection(cycleDataVector);
+    }
+
+    // search data directory
     DIR *dir;
     struct dirent *ent;
     int nextFileNo = getNextFileNo(cycleDataVector);
     int fileNameIndex = -1;
     char fileNameIndexStr[5];
-
-    // search data directory
-    string path = selectionPath;
-    if ((dir = opendir (dataDirName)) != NULL) {
+    if ((dir = opendir (dataSelectionDir.c_str())) != NULL) {
+        cout << "dataSelectionDir: " << dataSelectionDir << endl;
         while ((ent = readdir (dir)) != NULL){
             if(strstr(ent->d_name, "run")==NULL) continue;  // exclude other file
             strncpy(fileNameIndexStr, ent->d_name+3,4); // get file serial number
             fileNameIndexStr[4]=0;
             fileNameIndex = atoi(fileNameIndexStr);
+            // cout << "fileNameIndex: " << fileNameIndex << "  nextFileNo: " << nextFileNo << endl;
+            if(nextFileNo==237)
+                cout << endl << endl;
             if(fileNameIndex==nextFileNo){  // file id matched
                 nextFileNo = getNextFileNo(cycleDataVector);
                 for(unsigned i=0; i<cycleDataVector.size(); i++){  // search corresponding FileData
-                    vector<FileData> &vec = *cycleDataVector[i].fileDataVector;
+                    vector<FileData> &vec = cycleDataVector[i].fileDataVector;
                     for(unsigned j=0; j<vec.size(); j++){
                         if(vec[j].originalFileId==fileNameIndex){
                             vec[j].id = newIdCounter++;
@@ -194,8 +227,8 @@ void cycleExtract(vector<CycleData> &cycleDataVector)
                         }
                     }
                 }
-                fileNameVector.push_back(path + ent->d_name);
-                // cout << fileNameVector.back() << endl;
+                fileNameVector.push_back(dataSelectionDir + ent->d_name);
+                cout << "using file: " << ent->d_name << endl;
                 #ifdef COPY_FILE_TO_SELECTION
                 char cmd[512] = "copy /y .\\dp_variable_selection\\\"";  // copy to selection directory
                 strcat(cmd, ent->d_name);
@@ -213,29 +246,40 @@ void cycleExtract(vector<CycleData> &cycleDataVector)
     }
 
     // start file extraction
-    // i<fileNameVector.size()
-    for(unsigned i=0; i<2; i++){
-        singleFileExtract(fileNameVector[i].c_str());
+    //cout << "size of cycle[0] = " << cycleDataVector[0].fileDataVector.size() << endl;
+    //cout << cycleDataVector[0].fileDataVector[0].fileName << endl;
+    //singleFileExtract(cycleDataVector[0].fileDataVector[0].fileName.c_str(), cycleDataVector[0].fileDataVector[0]);
+    for(unsigned i=0; i<1; i++){ // for each cycle
+        cout << "Cycle = " << cycleDataVector[i].cycle << endl;
+        for(unsigned j=0; j<cycleDataVector[i].fileDataVector.size(); j++){ // for each file
+            singleFileExtract(cycleDataVector[i].fileDataVector[j].fileName.c_str(),
+                              cycleDataVector[i].fileDataVector[j]);
+        }
     }
     printf("Finished !\n");
 
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    vector<CycleData> cycleDataVector;
-    cycleExtract(cycleDataVector);
+    vector<CycleData> cycleDataVector; // database
+
+    bool doCopy = false;
+    if(argc==2 && strcmp(argv[1],"init")==0)
+        doCopy = false;
+    cycleExtract(cycleDataVector, doCopy);
+
     FileData fd = getFileById(cycleDataVector, 0);
-    cout << "FileName = \"" << fd.fileName << "\"" << endl;
+    cout << "fileName = \"" << fd.fileName << "\"" << endl;
     cout << "originalFileId = " << fd.originalFileId << endl;
+    cout << "id = " << fd.id << endl;
     cout << fd.dataVector.size() << endl;
-    /*for(unsigned i=0; i<fd.dataVector->size(); i++){
-        cout << "fd.dataVector->size() = " << fd.dataVector->size() << endl;
-        cout << "(*fd.dataVector)[i].size() = " << (*fd.dataVector)[i].size() << endl;
-        for(unsigned j=0; j<(*fd.dataVector)[i].size(); j++){
-            cout << (*fd.dataVector)[i][j] << " ";
+    for(unsigned i=0; i<100; i++){ // each line
+            cout << fd.timeStamp[i] << endl;
+        for(unsigned j=0; j<fd.dataVector[i].size(); j++){
+            cout << fd.dataVector[i][j] << " ";
         }
         cout << endl;
-    }*/
+    }
     return 0;
 }
